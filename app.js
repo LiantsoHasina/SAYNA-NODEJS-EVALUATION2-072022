556427226b5b69b1b88dacb9357f68bd6e18bb84
LiantsoHasina <Liantsorak@gmail.com>
2022-08-01 12:44:43 +0300

    My final fOrm

--- app.js
+++ app.js
@@ -1,43 +1,131 @@
 const express = require('express')
+const path = require('path')
+const ejs = require('ejs')
+const eLayouts = require('express-ejs-layouts')
+
+// fetch pool
+const database = require('./public/js/db.js')
 const app = express()
 const port = 3000
 
-app.use(express.static('public'))
-app.use('/css', express.static(__dirname + 'public/css'))
-app.use('/image', express.static(__dirname + 'public/image'))
-app.use('/js', express.static(__dirname + 'public/js'))
+
+
+app
+    // Static file access
+    .use(express.static('public'))
+    .use('/css', express.static(__dirname + 'public/css'))
+    .use('/image', express.static(__dirname + 'public/image'))
+    .use('/js', express.static(__dirname + 'public/js'))
+
+
+    .use(express.urlencoded({extended:false}))
+    .use(eLayouts)
+
+    // set up ejs files
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+
+    
+
+    
 
 // home page
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/index.html')
+    
+    database.getConnection((err, connection)=>{
+        if (err) throw err
+
+        connection.execute("SELECT firstName, testimony, note FROM forms WHERE note>=7 ORDER BY note DESC", (err,result)=>{
+            res.render(`${__dirname + '/views/index.ejs'}`, {title: "Home page", home:result})
+        })
+    })
+    
 })
 
 // backend page
 app.get('/backend', (req, res) => {
-    res.sendFile(__dirname + '/views/backend.html')
+    database.getConnection((err, connection)=>{
+        if (err) throw err
+
+        connection.execute("SELECT id_forms, firstName, testimony, note FROM forms WHERE modules='Backend' ORDER BY id_forms DESC", (err,result)=>{
+            res.render(`${__dirname + '/views/backend.ejs'}`, {title: "Back-End", back:result})
+        })
+    })
 })
 
 // frontend page
 app.get('/frontend', (req, res) => {
-    res.sendFile(__dirname + '/views/frontend.html')
+    database.getConnection((err, connection)=>{
+        if (err) throw err
+
+        connection.execute("SELECT id_forms, firstName, testimony, note FROM forms WHERE modules='Frontend' ORDER BY id_forms DESC", (err,result)=>{
+            res.render(`${__dirname + '/views/frontend.ejs'}`, {title: "Front-end", front:result})
+        })
+    })
 })
 
 // DM page
 app.get('/marketing', (req, res) => {
-    res.sendFile(__dirname + '/views/marketing.html')
+    database.getConnection((err, connection)=>{
+        if (err) throw err
+
+        connection.execute("SELECT id_forms, firstName, testimony, note FROM forms WHERE modules='Marketing' ORDER BY id_forms DESC", (err,result)=>{
+            res.render(`${__dirname + '/views/marketing.ejs'}`, {title: "Digital Marketing", dm:result})
+        })
+    })
 })
 
 // ux/ui
 app.get('/uxui', (req, res) => {
-    res.sendFile(__dirname + '/views/uxui.html')
+    
+    database.getConnection((err, connection)=>{
+        if (err) throw err
+
+        connection.execute("SELECT id_forms, firstName, testimony, note FROM forms WHERE modules='UX-UI' ORDER BY id_forms DESC", (err,result)=>{
+            res.render(`${__dirname + '/views/uxui.ejs'}`, {title: "UX - UI", design:result})
+        })
+    })
 })
 
-// contract
+// contact
 app.get('/contact', (req, res) => {
-    res.sendFile(__dirname + '/views/contact.html')
+    res.render(`${__dirname + '/views/contact.ejs'}`, {title: "Contact"})
+
+})
+
+// sign up
+app.get('/signup', (req, res) => {
+    res.render(`${__dirname + '/views/signup.ejs'}`, {title: "Sign up"})
+})
+
+app.post("/action_page", (req, res) => {
+    let firstName = req.body.firstname
+    let lastName = req.body.lastname
+    let avis = req.body.avis
+    let note = req.body.note
+    let formation = req.body.formation 
+
+    database.getConnection((err, connection) => {
+        if(err) throw err;
+        let userValue = [null, firstName, lastName, avis, note, formation]
+        connection.query("INSERT INTO forms VALUES (?, ?, ?, ?, ?, ?)", 
+        userValue, (err,result) => {
+            if (err) {
+                throw err;
+            } else{
+                res.redirect("/")
+            }
+
+        })
+    })
+    
+
 })
 
 
 
 
 app.listen(port, () => console.log(`Live on port ${port}`))
+
+
+module.exports = database
--- public/js/db.js
+++ public/js/db.js
@@ -1,6 +1,8 @@
 require('dotenv').config()
 const mysql = require ('mysql2')
 
+
+
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
@@ -8,13 +10,5 @@ const pool = mysql.createPool({
     database: process.env.DB_NAME,
     password: process.env.DB_PASSWORD
 })
-
-let sql = "SELECT * FROM test;"
-
-pool.execute(sql, function(err, res) {
-    if (err) throw err;
-
-    console.log(res)
-})
-
-module.exports = pool.promise()
+// Pour pouvoir utiliser la bd ailleurs
+module.exports = pool
